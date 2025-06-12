956ab8a8cc2e29aec8eb5aa1bf02ebc4a6d22a57
Weslei BBS <wesleibbs@gmail.com>
2025-06-12 17:10:33 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/porta-retrato-digital/sw.js', { scope: '/porta-retrato-digital/' })})}
